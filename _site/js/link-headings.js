64b9d145f5d640f9b8971cbaf3710b6a38fe2c4c
Brad Frost <brad@bradfrost.com>
2016-10-30 11:36:44 -0700

    Change scope of subheadings

--- _site/js/link-headings.js
+++ _site/js/link-headings.js
@@ -1,3 +1,3 @@
-$('h2,h3,h4,h5').filter('[id]').each(function () {
+$('.text h2, .text h3, .text h4, .text h5').filter('[id]').each(function () {
     $(this).html('<a href="#'+$(this).attr('id')+'" class="title-link">' + '#' + '</a>' + $(this).text());
 });
