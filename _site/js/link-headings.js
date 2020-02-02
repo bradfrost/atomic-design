3a1f606b8d3a4f1f2e4d087bfd7b86c49b02215a
Brad Frost <brad@bradfrost.com>
2020-02-02 15:44:43 -0500

    Remove _site from source

--- _site/js/link-headings.js
+++
@@ -1,3 +0,0 @@
-$('.text h2, .text h3, .text h4, .text h5').filter('[id]').each(function () {
-    $(this).html('<a href="#'+$(this).attr('id')+'" class="title-link">' + '#' + '</a>' + $(this).text());
-});
