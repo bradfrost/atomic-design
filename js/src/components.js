3c7cc3069d0e470293111e81dcf41097bf8a0ad9
Ian Frost <inf5000@gmail.com>
2025-11-26 15:10:01 -0500

    Get modal to work no localStorage yet

---
+++ js/src/components.js
@@ -0,0 +1,6 @@
+// Import all Eddie web components
+import '@brad-frost-web/eddie-web-components/components/black-friday-modal/black-friday-modal.js';
+import '@brad-frost-web/eddie-web-components/components/heading/heading.js';
+import '@brad-frost-web/eddie-web-components/components/text-passage/text-passage.js';
+import '@brad-frost-web/eddie-web-components/components/layout-container/layout-container.js';
+
