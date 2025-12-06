e4e4772d4cd895b2e18985a07cd8735e74692ee9
Ian Frost <inf5000@gmail.com>
2025-12-06 09:09:55 -0500

    Remove Black Friday modal. Change text of banner

--- js/src/components.js
+++ js/src/components.js
@@ -1,3 +1,2 @@
 // Import all Eddie web components
-import "@brad-frost-web/eddie-web-components/components/black-friday-modal/black-friday-modal.js";
 import "@brad-frost-web/eddie-web-components/components/black-friday-banner/black-friday-banner.js";
